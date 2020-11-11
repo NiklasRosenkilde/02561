ff4217106f33ffbfce65d1365780b68b1c453f03
Niklas Rosenkilde Rasmussen <niklas.rosenkilde@oulook.com>
2020-11-11 13:11:38 +0100

    Update W9-1.js

--- Worksheet_9/W9-1.js
+++ Worksheet_9/W9-1.js
@@ -7,7 +7,10 @@ var teapotProgram, groundProgram;
 var theta = 0;
 
 var aspect;
-var moveTeapot, lookDown, moveLight;
+var moveTeapot = true, lookDown = false, moveLight = true;
+
+var projectionMatrix;
+var lightX, lightY, lightZ;
 
 
 // GEOMETRY PARAMETERS ////////////////////////////////////////////////////////
@@ -22,41 +25,39 @@ var groundQuad = [
   vec3(2, -1, -1)
 ];
 var texCoords = [
-  vec2(-1,-1),
+  vec2(-1, -1),
   vec2(-1, 1),
-  vec2( 1, 1),
-  vec2( 1,-1)];
+  vec2(1, 1),
+  vec2(1, -1)];
 
 var indices = [0, 3, 2, 0, 2, 1];
-
-
 
 
 window.onload = function init() {
 
   initScene();
-
   initTexture();
 
-
   // CLICK FUNCTIONS /////////////////////////////////////////////////////////
-  var bt_moveTeapot = document.getElementById("moveTeapot").onchange = x => {
-      moveTeapot = bt_moveTeapot.checked;
+  var bt_moveTeapot = document.getElementById("moveTeapot");
+  bt_moveTeapot.onchange = x => {
+    moveTeapot = bt_moveTeapot.checked;
   };
 
-  var bt_lookDown = document.getElementById("lookDown").onchange = x => {
-      lookDown = bt_lookDown.checked;
+  var bt_lookDown = document.getElementById("lookDown");
+  bt_lookDown.onchange = x => {
+    lookDown = bt_lookDown.checked;
   };
 
-  var bt_moveLight = document.getElementById("moveLight").onchange = x => {
-      moveLight = bt_moveLight.checked;
+  var bt_moveLight = document.getElementById("moveLight");
+  bt_moveLight.onchange = x => {
+    moveLight = bt_moveLight.checked;
   };
 
   // Projection shadow matrix
+  lightY = 3;
 
-  let lightY = 3;
-
-  let projectionMatrix = mat4(1);
+  projectionMatrix = mat4(1);
   projectionMatrix[3][3] = 0;
   projectionMatrix[3][1] = 1 / -(lightY - (-1)); // -1 is the ground y
 
@@ -73,43 +74,40 @@ function initTexture() {
   var texture = gl.createTexture();
   image.crossorigin = 'anonymous';
   image.onload = e => {
-      console.log("Texture loaded");
-      
-      gl.activeTexture(gl.TEXTURE0);
+    console.log("Texture loaded");
 
-      gl.uniform1i(groundProgram.texture, 0);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
 
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    // gl.uniform1i(groundProgram.texture, 0);
 
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
 
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
 
-      //window.requestAnimationFrame(render);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    //window.requestAnimationFrame(render);
   };
   image.src = 'textures/xamp23.png';
 }
 
 function render(time) {
-  // background
-  gl.clearColor(0.53, 0.81, 1.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   // light
-  let t = time / 1000;
-  let lightX, lightZ;
+  var t = time / 1000;
 
   if (moveLight) {
-      lightX = Math.sin(t);
-      lightZ = -3 + Math.cos(t);
+    lightX = Math.sin(t);
+    lightZ = -3 + Math.cos(t);
   } else {
-      lightX = 0;
-      lightZ = -3;
+    lightX = 0;
+    lightZ = -3;
   }
 
   // RENDER GROUND
@@ -121,21 +119,21 @@ function render(time) {
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, groundModel.indexBuffer);
 
   // common perspective + lookAt
-  let perspectiveMatrix = [
-      perspective(90, 1, 1, 20),
-      lookAt(lookDown ? vec3(0, 2, -2.99) : vec3(0, 0, 0), vec3(0, 0, -3), vec3(0, 1, 0)),
+  var perspectiveMatrix = [
+    perspective(90, 1, 1, 20),
+    lookAt(lookDown ? vec3(0, 2, -2.99) : vec3(0, 0, 0), vec3(0, 0, -3), vec3(0, 1, 0)),
   ].reduce(mult);
 
   // ground perspective
   {
-      let uLocation = gl.getUniformLocation(groundProgram, 'perspective');
-      gl.uniformMatrix4fv(uLocation, false, flatten(perspectiveMatrix));
+    var uLocation = gl.getUniformLocation(groundProgram, 'perspective');
+    gl.uniformMatrix4fv(uLocation, false, flatten(perspectiveMatrix));
   }
 
   // ground modelView
   {
-      let uLocation = gl.getUniformLocation(groundProgram, 'modelView');
-      gl.uniformMatrix4fv(uLocation, false, flatten(mat4()));
+    var uLocation = gl.getUniformLocation(groundProgram, 'modelView');
+    gl.uniformMatrix4fv(uLocation, false, flatten(mat4()));
   }
   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
 
@@ -147,61 +145,63 @@ function render(time) {
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, teapotModel.indexBuffer);
 
   if (!g_drawingInfo && g_objDoc && g_objDoc.isMTLComplete()) {
-      // OBJ and all MTLs are available
-      g_drawingInfo = onReadComplete(gl, teapotModel, g_objDoc);
-      console.log("g_drawingInfo set!");
-      console.log(g_drawingInfo.indices.length);
+    // OBJ and all MTLs are available
+    g_drawingInfo = onReadComplete(gl, teapotModel, g_objDoc);
+    console.log("g_drawingInfo set!");
+    // console.log(g_drawingInfo.indices.length);
   }
 
   if (!g_drawingInfo) {
-      console.log('OBJ not loaded!');
-  } else {
-      let transY = moveTeapot ? 0.25 * Math.cos(t) : 0;
+    console.log('OBJ not loaded!');
+  }
+  else {
+    var transY = moveTeapot ? 0.25 * Math.cos(t) : 0;
 
-      // view
-      var modelViewMatrix = [
-          translate(0, transY, -3),
-          scalem(0.25, 0.25, 0.25),
+    // view
+    var modelViewMatrix = [
+      translate(0, transY, -3),
+      scalem(0.25, 0.25, 0.25),
+    ].reduce(mult);
+
+    // FIRST, THE SHADOWS
+    {
+      var modelView = [
+        translate(0, -0.001, 0),
+        translate(lightX, lightY, lightZ),
+        projectionMatrix,
+        translate(-lightX, -lightY, -lightZ),
+        modelViewMatrix
       ].reduce(mult);
 
-      // FIRST, THE SHADOWS
-      {
-          let modelView = [
-              translate(0, -0.001, 0),
-              translate(lightX, lightY, lightZ),
-              projectionMatrix,
-              translate(-lightX, -lightY, -lightZ),
-              modelViewMatrix
-          ].reduce(mult);
+      var uLocation = gl.getUniformLocation(teapotProgram, 'modelView');
+      gl.uniformMatrix4fv(uLocation, false, flatten(modelView));
 
-          let uLocation = gl.getUniformLocation(teapotProgram, 'modelView');
-          gl.uniformMatrix4fv(uLocation, false, flatten(modelView));
-
-          // shadow mode
-          let visLocation = gl.getUniformLocation(teapotProgram, 'visible');
-          gl.uniform1f(visLocation, false);
-          gl.depthFunc(gl.GREATER);
-
-          gl.drawElements(gl.TRIANGLES, g_drawingInfo.indices.length, gl.UNSIGNED_SHORT, 0);
-      }
-
-      // THEN, THE TEAPOT
-      {
-          let uLocation = gl.getUniformLocation(teapotProgram, 'modelView');
-          gl.uniformMatrix4fv(uLocation, false, flatten(modelViewMatrix));
-      } {
-          let uLocation = gl.getUniformLocation(teapotProgram, 'perspectiveMatrix');
-          gl.uniformMatrix4fv(uLocation, false, flatten(perspectiveMatrix));
-      }
-
-      // normal mode
-      gl.depthFunc(gl.LESS);
-      let visLocation = gl.getUniformLocation(teapotProgram, 'visible');
-      gl.uniform1f(visLocation, true);
+      // shadow mode
+      var visLocation = gl.getUniformLocation(teapotProgram, 'visible');
+      gl.uniform1f(visLocation, false);
+      gl.depthFunc(gl.GREATER);
 
       gl.drawElements(gl.TRIANGLES, g_drawingInfo.indices.length, gl.UNSIGNED_SHORT, 0);
+    }
+
+    // THEN, THE TEAPOT
+    {
+      var uLocation = gl.getUniformLocation(teapotProgram, 'modelView');
+      gl.uniformMatrix4fv(uLocation, false, flatten(modelViewMatrix));
+    } {
+      var uLocation = gl.getUniformLocation(teapotProgram, 'perspectiveMatrix');
+      gl.uniformMatrix4fv(uLocation, false, flatten(perspectiveMatrix));
+    }
+
+    // normal mode
+    gl.depthFunc(gl.LESS);
+    var visLocation = gl.getUniformLocation(teapotProgram, 'visible');
+    gl.uniform1f(visLocation, true);
+
+    gl.drawElements(gl.TRIANGLES, g_drawingInfo.indices.length, gl.UNSIGNED_SHORT, 0);
   };
-  window.requestAnimationFrame(render);
+
+  requestAnimationFrame(render);
 }
 
 ////////////////////// INIT SCENE FUNCTION /////////////////////////////////
@@ -209,7 +209,8 @@ function initScene() {
   //Grabs stuff from html
   canvas = document.getElementById("c");
 
-  gl = WebGLDebugUtils.makeDebugContext(canvas.getContext("webgl") );
+  // gl = WebGLDebugUtils.makeDebugContext(canvas.getContext("webgl") );
+  gl = WebGLUtils.setupWebGL(canvas);
 
   aspect = canvas.width / canvas.height;
 
@@ -218,15 +219,12 @@ function initScene() {
   gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.enable(gl.DEPTH_TEST);
-  gl.enable(gl.CULL_FACE);
-  gl.cullFace(gl.BACK);
 
   gl.enable(gl.BLEND);
   gl.blendEquation(gl.FUNC_ADD);
   gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
   // Init teapot shaders
-
   teapotProgram = initShaders(gl, "vertex-shader-teapot", "fragment-shader-teapot");
   gl.useProgram(teapotProgram);
 
@@ -248,9 +246,9 @@ function initScene() {
   groundProgram = initShaders(gl, "vertex-shader-ground", "fragment-shader-ground");
   gl.useProgram(groundProgram);
 
-  groundProgram.position = gl.getAttribLocation(groundProgram, 'position');
+  groundProgram.position    = gl.getAttribLocation(groundProgram, 'position');
   groundProgram.texPosition = gl.getAttribLocation(groundProgram, 'texPosition');
-  groundProgram.texture = gl.getUniformLocation(groundProgram, 'texture');
+  groundProgram.texture     = gl.getUniformLocation(groundProgram, 'texture');
 
   groundModel.vertexBuffer = createEmptyArrayBuffer(gl, groundProgram.position, 3, gl.FLOAT);
   gl.bufferData(gl.ARRAY_BUFFER, flatten(groundQuad), gl.STATIC_DRAW);
@@ -262,6 +260,21 @@ function initScene() {
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, groundModel.indexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
 
+}
+
+////////////////////// BUFFER FUNCTIONS //////////////////////////////////
+function createEmptyArrayBuffer(gl, a_attribute, num, type) {
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  gl.enableVertexAttribArray(a_attribute);
+  return buffer;
+}
+
+function initAttributeVariable(gl, a_attribute, buffer, num) {
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(a_attribute, num, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_attribute);
 }
 
 
@@ -287,7 +300,6 @@ function onReadOBJFile(fileString, fileName, scale, reverse) {
   }
 }
 
-
 // OBJ File has been read completely
 function onReadComplete(gl, model, objDoc) {
 
@@ -310,18 +322,4 @@ function onReadComplete(gl, model, objDoc) {
   return drawingInfo;
 }
 
-
-function createEmptyArrayBuffer(gl, a_attribute, num, type) {
-  var buffer = gl.createBuffer(); // Create a buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
-  gl.enableVertexAttribArray(a_attribute); // Enable the assignment
-  return buffer;
-}
-
-function initAttributeVariable(gl, a_attribute, buffer, num) {
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.vertexAttribPointer(a_attribute, num, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_attribute);
-}
 
